f60cc42d666608185b2c08e03155170780cf4072
Foltinium <local@localhost>
2026-04-07 11:29:38 +0300

    Добавление подсказок по заданию функционала элементам меню

--- src/ru/gr0946x/ui/Menu.java
+++ src/ru/gr0946x/ui/Menu.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 
 public class Menu {
     private final MainWindow window;
+//    поля ваших объектов
 
-    public Menu(MainWindow window) {
+    public Menu(MainWindow window /*ваши объекты-параметры*/) {
         this.window = window;
+//        this.поле = параметр
         createMenu();
     }
 
@@ -34,10 +36,20 @@ public class Menu {
 
         JMenu fileSave = new JMenu("Сохранить как...");
         JMenuItem saveAsPNG = new JMenuItem("Файл .png");
+//        пример добавления функционала элементу меню:
+//        saveAsPNG.addActionListener(e -> System.out.println("Сохранение успешно!"));
+
         JMenuItem saveAsJPG = new JMenuItem("Файл .jpg");
+//        saveAsJPG.addActionListener(e -> /*поле.метод()*/);
+
         JMenuItem saveAsFrac = new JMenuItem("Файл .frac");
+//        saveAsFrac.addActionListener(e -> /*поле.метод()*/);
 
         JMenuItem openFile = new JMenuItem("Открыть...");
+//        openFile.addActionListener(e -> /*поле.метод()*/);
+
+        JMenuItem createAnimation = new JMenuItem("Создать анимацию...");
+//        createAnimation.addActionListener(e -> /*поле.метод()*/);
 
         fileMenu.add(fileSave);
         fileSave.add(saveAsPNG);
@@ -45,13 +57,17 @@ public class Menu {
         fileSave.add(saveAsFrac);
 
         fileMenu.add(openFile);
+        fileMenu.addSeparator();
+        fileMenu.add(createAnimation);
 
         return fileMenu;
     }
 
     private JMenu createEditMenu() {
         JMenu editMenu = new JMenu("Правка");
-        JMenuItem undo = new JMenuItem("Отменить [Ctrl + Z]");
+        JMenuItem undo = new JMenuItem("Отменить");
+        undo.setAccelerator(KeyStroke.getKeyStroke("control Z"));
+//        undo.addActionListener(e -> /*поле.метод()*/);
 
         editMenu.add(undo);
 
@@ -61,20 +77,30 @@ public class Menu {
     private JMenu createViewMenu() {
         JMenu viewMenu  = new JMenu("Вид");
         JMenuItem juliaSet = new JMenuItem("Показать множество Жюлиа");
-        JMenu setFractalFunc = new JMenu("Задать функцию построения фрактала");
-        JMenu setColorScheme = new JMenu("Задать цветовую схему");
+//        juliaSet.addActionListener(e -> /*поле.метод()*/);
 
+        JMenu setFractalFunc = new JMenu("Задать функцию построения фрактала");
         JMenuItem fractalFuncA = new JMenuItem("Функция 1");
-//        fractalFuncA.addActionListener(e -> System.out.println("test"));
+//        fractalFuncA.addActionListener(e -> /*поле.метод()*/);
 
         JMenuItem fractalFuncB = new JMenuItem("Функция 2");
-        JMenuItem fractalFuncC = new JMenuItem("Функция 3");
+//        fractalFuncB.addActionListener(e -> /*поле.метод()*/);
 
+        JMenuItem fractalFuncC = new JMenuItem("Функция 3");
+//        fractalFuncC.addActionListener(e -> /*поле.метод()*/);
+
+        JMenu setColorScheme = new JMenu("Задать цветовую схему");
         JMenuItem colorSchemeA = new JMenuItem("Схема 1");
+//        colorSchemeA.addActionListener(e -> /*поле.метод()*/);
+
         JMenuItem colorSchemeB = new JMenuItem("Схема 2");
+//        colorSchemeB.addActionListener(e -> /*поле.метод()*/);
+
         JMenuItem colorSchemeC = new JMenuItem("Схема 3");
+//        colorSchemeC.addActionListener(e -> /*поле.метод()*/);
 
         viewMenu.add(juliaSet);
+        viewMenu.addSeparator();
         viewMenu.add(setFractalFunc);
         viewMenu.add(setColorScheme);
 
